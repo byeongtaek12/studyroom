806f071f51a388b663895d1c1e45934cbc3f63bd
obt <byeongtaek12@naver.com>
2025-09-21 11:43:54 +0900

    feat: customFilter 코드 추가
    인증 과정에서 에러 발생시 401 상태코드 반환

--- src/main/java/com/example/studyroom/common/CustomFilter.java
+++ src/main/java/com/example/studyroom/common/CustomFilter.java
@@ -2,8 +2,11 @@ package com.example.studyroom.common;
 
 import java.io.IOException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -31,6 +34,7 @@ public class CustomFilter extends OncePerRequestFilter {
 			filterChain.doFilter(request,response);
 		} catch (Exception e) {
 			log.warn("에러 발생: {}", e.getMessage());
+			throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "정상적인 토큰이 아닙니다");
 		}
 	}
 }
